c439aba8dbd9fa52198bbe53070c7c97e233e78d
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-20 00:32:19 -0400

    update tests/fixtures for 2.0

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
 'use strict';
 
-/* deps: mocha */
+require('mocha');
 var assert = require('assert');
-var pkg = require('./');
+var loadPkg = require('./');
 
 it('should load the package.json for this project', function () {
+  var pkg = loadPkg();
   assert.equal(pkg.name, 'load-pkg');
   assert.equal(pkg.license, 'MIT');
+});
+
+it('should load the package.json from the specified directory', function () {
+  var pkg = loadPkg('fixtures');
+  assert.equal(pkg.name, 'abc');
+});
+
+it('should load the package.json from the specified filepath', function () {
+  var pkg = loadPkg('fixtures/package.json');
+  assert.equal(pkg.name, 'abc');
 });
